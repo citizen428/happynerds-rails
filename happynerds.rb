0291e8cbcefb81c8c6d9cf76c803408ff707661f
Michael Kohl <citizen428@gmail.com>
2010-08-28 14:28:26 +0200

    slave_ok

--- happynerds.rb
+++ happynerds.rb
@@ -5,11 +5,11 @@ require 'sinatra'
 require 'haml'
 require 'mongo'
 
-set :root, APP_ROOT
+#set :root, APP_ROOT
 
 include Mongo
 
-db = Connection.new(ENV['DATABASE_URL'], ENV['DATABASE_PORT']).db('happynerds')
+db = Connection.new(ENV['DATABASE_URL'], ENV['DATABASE_PORT'], :slave_ok => true).db('happynerds')
 if ENV['DATABASE_USER'] && ENV['DATABASE_PASSWORD']
   auth = db.authenticate(ENV['DATABASE_USER'], ENV['DATABASE_PASSWORD'])
 end
@@ -33,5 +33,4 @@ end
 get '/*' do
   redirect '/'
 end
-
 
