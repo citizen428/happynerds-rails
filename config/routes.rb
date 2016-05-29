77b33ac29bbfbdb2d4f55b814b505d6c43eb9c91
Michael Kohl <citizen428@gmail.com>
2016-05-29 10:09:52 +0700

    Redirect for legacy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'high_voltage/pages#show', id: 'intro'
-  get '/view/:type' => 'sites#index', as: :site_index
+  # redirect legacy routes
+  get '/view/:type',to: redirect { |path_params, _req| "/sites?type=#{path_params[:type]}" }
+  resources :sites, only: [:index, :new, :create]
 end
