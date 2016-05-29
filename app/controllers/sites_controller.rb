59b6f190011659740e4ae5791d8e2e0696f46f06
Michael Kohl <citizen428@gmail.com>
2016-05-29 10:10:08 +0700

    HTTP auth

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,8 @@
 class SitesController < ApplicationController
+  http_basic_authenticate_with name: Rails.application.secrets.admin_user,
+                               password: Rails.application.secrets.admin_pw,
+                               except: :index
+
   def index
     @sites = Site.tagged(params[:type])
   end
