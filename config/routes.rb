495d4655c5a8388f6de64529fe3e1a393445c919
Michael Kohl <citizen428@gmail.com>
2016-07-18 19:33:00 +0700

    Update to Rails 5,switch to Puma

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'high_voltage/pages#show', id: 'intro'
   # redirect legacy routes
-  get '/view/:type',to: redirect { |path_params, _req| "/sites?type=#{path_params[:type]}" }
+  get '/view/:type', to: redirect { |path_params, _req| "/sites?type=#{path_params[:type]}" }
   resources :sites, only: [:index, :new, :create]
 end
